ebccf4d078fba8d0319841206d741ce2d9387432
Rostyslav_Andrusiv <rostik_andrusiv@ukr.net>
2023-01-11 10:49:18 +0200

    changed LocalDateTime in Firestore model to Date, added mapping to mapstruct

--- src/main/java/org/rostik/andrusiv/model/PersonFirestore.java
+++ src/main/java/org/rostik/andrusiv/model/PersonFirestore.java
@@ -1,29 +1,23 @@
 package org.rostik.andrusiv.model;
 
-
-
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 public class PersonFirestore implements Serializable {
-
-
     private String id;
-
     private String name;
     private int age;
     private boolean isActive;
     private double balance;
     private List<String> phoneNumbers;
-    private LocalDateTime dob;
+    private Date dob;
     private Address address;
 
     public PersonFirestore() {
     }
 
-    public PersonFirestore(String id, String name, int age, boolean isActive, double balance, List<String> phoneNumbers, LocalDateTime dob, Address address) {
+    public PersonFirestore(String id, String name, int age, boolean isActive, double balance, List<String> phoneNumbers, Date dob, Address address) {
         this.id = id;
         this.name = name;
         this.age = age;
@@ -82,11 +76,11 @@ public class PersonFirestore implements Serializable {
         this.phoneNumbers = phoneNumbers;
     }
 
-    public LocalDateTime getDob() {
+    public Date getDob() {
         return dob;
     }
 
-    public void setDob(LocalDateTime dob) {
+    public void setDob(Date dob) {
         this.dob = dob;
     }
 
@@ -96,19 +90,6 @@ public class PersonFirestore implements Serializable {
 
     public void setAddress(Address address) {
         this.address = address;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PersonFirestore that = (PersonFirestore) o;
-        return age == that.age && isActive == that.isActive && Double.compare(that.balance, balance) == 0 && Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(phoneNumbers, that.phoneNumbers) && Objects.equals(dob, that.dob) && Objects.equals(address, that.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age, isActive, balance, phoneNumbers, dob, address);
     }
 
     @Override
--- src/main/java/org/rostik/andrusiv/util/PersonMapper.java
+++ src/main/java/org/rostik/andrusiv/util/PersonMapper.java
@@ -1,14 +1,25 @@
 package org.rostik.andrusiv.util;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import org.rostik.andrusiv.model.PersonFirestore;
 import org.rostik.andrusiv.model.PersonMongo;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 @Mapper
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
-
+    @Mapping(source = "dob", target = "dob", qualifiedBy = LocalDateTimeToDate.class)
     PersonFirestore mapToFirestore(PersonMongo personMongo);
 
+    @LocalDateTimeToDate
+    static Date LocalDateTimeToData(LocalDateTime localDateTime) {
+        return java.util.Date
+                .from(localDateTime.atZone(ZoneId.systemDefault())
+                        .toInstant());
+    }
 }
